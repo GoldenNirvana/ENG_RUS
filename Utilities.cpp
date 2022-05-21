9b24ba0c378b87a84e09d8d7b88621eb0bee72c3
Ilya <svaikinilya@yandex.ru>
2022-05-21 15:33:37 +0300

    Before accept

--- Utilities.cpp
+++ Utilities.cpp
@@ -18,58 +18,56 @@ Dictionary commonDictionary(std::vector<Dictionary> &common)
 
   std::map<std::string, size_t> translates; // перевод и кол-во раз которое встретилось этого перевода
 
-  for (const auto &dictionary: common)       // Идём по всем словарям
+  Dictionary dictionary = *common.begin();
+  for (const auto &pair: dictionary)        // Берём каждое слово в словаре
   {
-    for (const auto &pair: dictionary)        // Берём каждое слово в словаре
+    if (result.search(pair.first) != result.end())
     {
-      if (result.search(pair.first) != result.end())
+      continue;
+    }
+    isCommon = true;
+    translates.clear();
+    for (const auto &dictionarySecond: common)        // С текущим словом опять идём по всем словарям
+    {
+      if (dictionarySecond.search(pair.first) == dictionarySecond.end()) // Проверяем его наличие во всех остальных
       {
-        continue;
+        isCommon = false;                   // Если хотя бы в одном словаре его нет -> слово не уникальное и его не будет добавлять в Result
+        break;
       }
-      isCommon = true;
-      translates.clear();
-      for (const auto &dictionarySecond: common)        // С текущим словом опять идём по всем словарям
+      else
       {
-        if (dictionarySecond.search(pair.first) == dictionarySecond.end()) // Проверяем его наличие во всех остальных
+        Item item = *dictionarySecond.search(
+          pair.first); // находим set в котором все переводы этого слова в данном словаре
+        for (const auto &word: *item.second)   // вставляем все эти слова в наши переводы
         {
-          isCommon = false;                   // Если хотя бы в одном словаре его нет -> слово не уникальное и его не будет добавлять в Result
-          break;
-        }
-        else
-        {
-          Item item = *dictionarySecond.search(
-            pair.first); // находим set в котором все переводы этого слова в данном словаре
-          for (const auto &word: *item.second)   // вставляем все эти слова в наши переводы
-          {
-            translates[word]++;
-          }
+          translates[word]++;
         }
       }
-      if (isCommon)         // если во всех нашлось это слово -> isCommon останется true и мы слово добавим в result
+    }
+    if (isCommon)         // если во всех нашлось это слово -> isCommon останется true и мы слово добавим в result
+    {
+      std::set<std::string> set; // set в котором будут только общие слова
+      for (const auto &translate: translates)
       {
-        std::set<std::string> set; // set в котором будут только общие слова
-        for (const auto &translate: translates)
+        if (translate.second == common.size())
         {
-          if (translate.second == common.size())
-          {
-            set.insert(translate.first);
-          }
+          set.insert(translate.first);
         }
-        if (set.empty()) // Тут будем давать написать перевод если не встретилось ни одного общего
+      }
+      if (set.empty()) // Тут будем давать написать перевод если не встретилось ни одного общего
+      {
+        std::cout << "Write translate for " + pair.first << '\n';
+        std::cout << "Write \"Exit\" to escape\n";
+        std::string in;
+        std::cin >> in;
+        while (in != "Exit")
         {
-          std::cout << "Write translate for " + pair.first << '\n';
-          std::cout << "Write \"Exit\" to escape\n";
-          std::string in;
+          set.insert(in);
           std::cin >> in;
-          while (in != "Exit")
-          {
-            set.insert(in);
-            std::cin >> in;
-          }
         }
-        std::shared_ptr<std::set<std::string>> sharedPtr = std::make_shared<std::set<std::string>>(set);
-        result.insert({pair.first, sharedPtr});
       }
+      std::shared_ptr<std::set<std::string>> sharedPtr = std::make_shared<std::set<std::string>>(set);
+      result.insert({pair.first, sharedPtr});
     }
   }
   return result;
